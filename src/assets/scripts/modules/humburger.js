cffbaeaa43b51b44a4e057ca0ef9fc24f6d7d796
Nurken Seitov <seitovnurken@gmail.com>
2018-07-10 11:38:51 +0600

    clone the works computer

--- src/assets/scripts/modules/humburger.js
+++ src/assets/scripts/modules/humburger.js
@@ -3,8 +3,8 @@ var card = document.querySelector(".overlay");
 var menuBg = document.querySelector(".hamburger__menu-bars");
 var bodyOverflow = document.querySelector("body");
 
-btn.addEventListener("click", function() {
-  event.preventDefault();
+btn.addEventListener("click", function(e) {
+  e.preventDefault();
   card.classList.toggle("overlay__open");
   menuBg.classList.toggle("hamburger__menu-bars-active");
   bodyOverflow.classList.toggle("overflow-hidden");
--- src/assets/scripts/modules/log-in-scene.js
+++ src/assets/scripts/modules/log-in-scene.js
@@ -1,7 +1,7 @@
 var btn = document.querySelector("#log-in__links");
 var card = document.querySelector(".welcome__content-wrap");
 
-btn.addEventListener("click", function() {
-  event.preventDefault();
+btn.addEventListener("click", function(e) {
+  e.preventDefault();
   card.classList.toggle("welcome__content-wrap__is-flipped");
 });
